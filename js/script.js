ecea2e22235e6fceead6ed0b1e66947637ebc7e3
Ali-Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2022-10-31 14:45:56 -0400

    save

--- js/script.js
+++ js/script.js
@@ -1,23 +1,26 @@
-// Copyright (c) 2020 ali-mugamai All rights reserved
+// Copyright (c) 2020 Ali Mugamai All rights reserved
 //
-// Created by: ali mugamai
-// Created on: Sep 2020
+// Created by: Ali Mugamai
+// Created on: oct 2022
 // This file contains the JS functions for index.html
 
 "use strict"
 
-/**
- * Check servie worker.
- */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS20-03-01-JS/sw.js", {
+    scope: "/ICS20-03-01-JS/",
   })
 }
-
 /**
- * This function displays an alert.
+ * This function calculates area and perimeter of rectangle.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function calculate() {
+  // input
+  const height = parseInt(document.getElementById("height").value)
+  const base = parseInt(document.getElementById("base").value)
+  const top = parseInt(document.getElementById("top").value)
+  // process
+  const area = ((base + top) / 2) * height
+  // output
+  document.getElementById("area").innerHTML = "Area is: " + area + " cm²"
 }
